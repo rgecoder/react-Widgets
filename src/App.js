537f55a95c923953c0972a70deed01c97e897706
Roy Ge <rgecoder@gmail.com>
2020-11-21 07:36:41 -0800

    v1.12.2 Add Dropdown component widget

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import React from "react";
+import React,{useState} from "react";
 import Accordion from "./components/Accordion";
-import Search from './components/Search';
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a front end JS Library'
+    title: "What is React?",
+    content: "React is a front end JS Library",
   },
   {
-    title:'Why use React?',
-    content: 'React is a favorite JS Library among engineers'
+    title: "Why use React?",
+    content: "React is a favorite JS Library among engineers",
   },
   {
-    title:'How do you use React?',
-    content: 'You use React by creating components'
-  }
-]
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  { label: "The Color Red", value: "red" },
+  { label: "The Color Green", value: "green" },
+  { label: "A Shade of Blue", value: "blue" },
+];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]); //default to 1st option
   return (
     <div>
-      
       {/* <Accordion  items={items}/> */}
 
-      <Search/>
+      {/* <Search/> */}
+
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
